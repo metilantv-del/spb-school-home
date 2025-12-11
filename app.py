1b0e5d02615dfeabb7e0d8f3ce8f89f6efe2e44d
metilantv-del <metilantv@gmail.com>
2025-12-11 12:47:23 +0300

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ STOPS_CONFIG = [
     {"name": "Замшина улица", "stop_id": "18872", "route_id": "295", "vehicle": "Автобус", "direction": "home"},
 ]
 
-GTFS_RT_URL = "GTFS_RT_URL = "https://transport.orgp.spb.ru/Portal/transport/internalapi/gtfs""
+GTFS_RT_URL = "GTFS_RT_URL = "transport.orgp.spb.ru/gtfs-rt/trip_updates.pb""
 TIMEZONE = pytz.timezone("Europe/Moscow")
 
 app = Flask(__name__, static_folder='.')
